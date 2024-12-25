bbdfd250162b9843eb2452d3ebce4cd82d0ba134
HuyTan <nghuytan.ptit@gmail.com>
2024-12-25 09:23:08 +0700

    2

--- backend/src/configs/database.js
+++ backend/src/configs/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'nghuytan',
-    password: '',
-    database: 'delivery_management'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
